a5a9954fed394a2930984c74d1b2b6da8c0638a1
azeupaul <azeupaul@gmail.com>
2022-10-28 00:52:56 +0100

    Remove unused import

--- app/src/main/java/com/azeupaul/diceroller/MainActivity.kt
+++ app/src/main/java/com/azeupaul/diceroller/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
